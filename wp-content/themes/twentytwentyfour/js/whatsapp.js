30127372aa9746d143476a949b2c9f23f7175c6c
Javier Rojas <jardlml1209@gmail.com>
2024-10-19 00:23:40 -0300

    first commit

---
+++ wp-content/themes/twentytwentyfour/js/whatsapp.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Array con los números de WhatsApp
+    const wspNumbers = [
+        '56948764112',  
+        '56973893988' 
+    ];
+
+    // Selecciona el botón de WhatsApp 
+    const wspButton = document.querySelector('.qlwapp-toggle');
+
+    if (wspButton) {
+
+        wspButton.addEventListener('click', function(event) {
+            event.preventDefault(); // Evita el comportamiento predeterminado del botón
+            const randomIndex = Math.floor(Math.random() * wspNumbers.length);
+            const selectedNumber = wspNumbers[randomIndex];
+            wspButton.setAttribute('data-phone', selectedNumber); // Cambia el número de teléfono
+            const message = '¡Hola! Me gustaría obtener más información.';
+            const wspUrl = `https://wa.me/${selectedNumber}?text=${encodeURIComponent(message)}`;
+            window.open(wspUrl, '_blank'); // Abre WhatsApp con el nuevo número
+        });
+    }
+});
